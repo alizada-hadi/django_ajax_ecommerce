2efb48e58a63f9462e20b65912092795a627ff42
Hadi <alizadahadi2050@gmail.com>
2022-06-04 17:48:58 +0430

    recent changes

--- myshop/urls.py
+++ myshop/urls.py
@@ -5,10 +5,10 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path("", include("orders.urls", namespace="orders")),
     path('admin/', admin.site.urls),
     path("cart/", include("cart.urls", namespace="cart")),
     path("payment/", include("payment.urls", namespace="payment")),
-    path("", include("orders.urls", namespace="orders")),
     path("", include("shop.urls", namespace="shop")),
 ]
 
--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, redirect
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import render, redirect, get_object_or_404
 from itertools import product
 from .models import Order, OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from django.urls import reverse
 # Create your views here.
+
+
+
 
 def order_create(request):
     cart = Cart(request)
@@ -28,3 +32,8 @@ def order_create(request):
         form = OrderCreateForm()
 
     return render(request, "orders/order/create.html", {"cart":cart, "form" : form})
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, "admin/orders/order/detail.html", {"order": order})
